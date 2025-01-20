673c3de5ac1ad69a205b852a47c35d26beaa5c9b
Ziqi Wang <wangziqi2385823041@outlook.com>
2025-01-20 21:17:12 +0200

    build: add containerized config for frontend and backend 1. dockerfile and docker compose 2. temporarily remove the db connection part in backend for testing purpose

--- backend/app/db/db.py
+++ backend/app/db/db.py
@@ -1,14 +1,13 @@
-from sqlmodel import create_engine, SQLModel
-import os
-from dotenv import load_dotenv
-import app.models
-load_dotenv()
+# from sqlmodel import create_engine, SQLModel
+# import os
+# from dotenv import load_dotenv
+# load_dotenv()
 
-database_url = os.getenv("DB_URL")
-engine = create_engine(url=database_url, echo=True)
+# database_url = os.getenv("DB_URL")
+# engine = create_engine(url=database_url, echo=True)
 
-def create_db_and_tables():
-    SQLModel.metadata.create_all(engine)
+# def create_db_and_tables():
+#     SQLModel.metadata.create_all(engine)
 
-if __name__ == "__main__":
-    create_db_and_tables()
+# if __name__ == "__main__":
+#     create_db_and_tables()
--- backend/app/main.py
+++ backend/app/main.py
@@ -8,22 +8,23 @@ from fastapi import Depends, FastAPI, HTTPException, Query
 from fastapi.security import OAuth2PasswordBearer
 from fastapi.middleware.cors import CORSMiddleware
 
-from sqlmodel import SQLModel
-from .models.user import Moviebuddies
-from .db.db import engine
-from .db.db import create_db_and_tables
-from .api.v1.endpoints import user
+# from sqlmodel import SQLModel
+# from .models.user import Moviebuddies
+# from .db.db import engine
+# from .db.db import create_db_and_tables
+# from .api.v1.endpoints import user
 
 load_dotenv()
 
-@asynccontextmanager
-async def initialization(app: FastAPI):
-    create_db_and_tables()
-    yield
-    engine.dispose()
+# @asynccontextmanager
+# async def initialization(app: FastAPI):
+#     create_db_and_tables()
+#     yield
+#     engine.dispose()
 
-app = FastAPI(lifespan=initialization)
-app.include_router(user.router, prefix="/api/v1", tags=["user"])
+#app = FastAPI(lifespan=initialization)
+app = FastAPI()
+#app.include_router(user.router, prefix="/api/v1", tags=["user"])
 app.add_middleware(
   CORSMiddleware,
   allow_origins = ["*"],
