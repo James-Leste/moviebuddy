249ac645e11b63f141414f51f8d26b275c317459
Ziqi Wang <wangziqi2385823041@outlook.com>
2025-07-12 22:27:23 +0300

    api: add tv search endpoints

--- backend/app/api/v1/endpoints/movie_search.py
+++ backend/app/api/v1/endpoints/movie_search.py
@@ -17,7 +17,9 @@ from app.api.v1.endpoints import user
 load_dotenv()
 
 MOVIES_SEARCH_BASE_URL = 'https://api.themoviedb.org/3/search/movie'
+TVS_SEARCH_BASE_URL = 'https://api.themoviedb.org/3/search/tv'
 MOVIE_SEARCH_BASE_URL = 'https://api.themoviedb.org/3/movie'
+TV_SEARCH_BASE_URL = 'https://api.themoviedb.org/3/tv'
 
 TMDB_API_KEY = os.getenv("TMDB_API_KEY")
 
@@ -35,10 +37,30 @@ def search_movie_by_name(id: str):
     response = requests.get(f"{MOVIE_SEARCH_BASE_URL}/{id}?api_key={TMDB_API_KEY}")
     return response.json()
 
+def search_tvs(name: str, page: int | None = 1):
+    if not TMDB_API_KEY:
+        raise HTTPException(status_code=500, detail="API key not found")
+    response = requests.get(f"{TVS_SEARCH_BASE_URL}?query={name}&api_key={TMDB_API_KEY}&page={page}")
+    return response.json()
+
+def search_tv_by_name(id: str):
+    if not TMDB_API_KEY:
+        raise HTTPException(status_code=500, detail="API key not found")
+    response = requests.get(f"{TV_SEARCH_BASE_URL}/{id}?api_key={TMDB_API_KEY}")
+    return response.json()
+
 @router.get('/movie/search')
 def get_movies(name: str, page: int | None = 1):
     return search_movies(name, page)
 
 @router.get('/movie/{id}')
+def get_movies(id: str):
+    return search_movie_by_name(id)
+
+@router.get('/tv/search')
+def get_movies(name: str, page: int | None = 1):
+    return search_movies(name, page)
+
+@router.get('/tv/{id}')
 def get_movies(id: str):
     return search_movie_by_name(id)
